0b5dd7c07ab03f871541ff258faf36643be2f8b2
Rheitheillaethor <minegocu3@gmail.com>
2021-10-13 23:24:50 +0200

    The results are rounded, I used try/catch to prevent crashing program by giving strings instead of int/float. I have added "Again" method in order to loop program for convenience of using.

--- TemperatureConverter/Program.cs
+++ TemperatureConverter/Program.cs
@@ -6,54 +6,90 @@ namespace TemperatureConverter
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, what temperature scale do you want to convert from?");
             GetInfo();
-            
+            Again();
         }
         static void GetInfo()
         {
-            Console.WriteLine("Hello, what temperature scale do you want to convert from?");
             Console.WriteLine("'1' - Celsius");
             Console.WriteLine("'2' - Kelvin");
             Console.WriteLine("'3' - Fahrenheit");
-            int choice = int.Parse(Console.ReadLine());      
-            switch (choice)
+            string GivenInfo = Console.ReadLine();
+            try
             {
-                case 1:
-                    Console.WriteLine("You choosed Celsius");
-                    Convert(1);
-                    break;
-                case 2:
-                    Console.WriteLine("You choosed Kelvin");
-                    Convert(2);
-                    break;
-                case 3:
-                    Console.WriteLine("You choosed Fahrenheit");
-                    Convert(3);
-                    break;
-                default:
-                    Console.WriteLine("You choosed wrong number, try again");
-                    GetInfo();
-                    break;
+                int choice = int.Parse(GivenInfo);
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("You choosed Celsius");
+                        Convert(1);
+                        break;
+                    case 2:
+                        Console.WriteLine("You choosed Kelvin");
+                        Convert(2);
+                        break;
+                    case 3:
+                        Console.WriteLine("You choosed Fahrenheit");
+                        Convert(3);
+                        break;
+                    default:
+                        Console.WriteLine("You choosed wrong number, try again");
+                        GetInfo();
+                        break;
+                }
             }
+            catch (FormatException)
+            {
+                Console.WriteLine("{0} is not an integer" , GivenInfo);
+                Console.WriteLine("Try again!");
+                GetInfo();
+            }
+
         }
         static void Convert(int Number)
         {
             Console.WriteLine("enter the number of degrees: ");
-            float degrees = float.Parse(Console.ReadLine());
-            switch (Number)
+            string GivenDegrees = Console.ReadLine();
+            try
             {
-                case 1:
-                    Console.WriteLine(degrees + " Celsius are " + (degrees + 273.15) + " Kelvin");
-                    Console.WriteLine(degrees + " Celsius are " + (degrees * 1.8 + 32) + " Fahrenheit");
-                    break;
-                case 2:
-                    Console.WriteLine(degrees + " Kelvin are " + (degrees - 273.15) + " Celsius");
-                    Console.WriteLine(degrees + " Kelvin are " + ((degrees - 273.15) * 1.8 + 32) + " Fahrenheit");
-                    break;
-                case 3:
-                    Console.WriteLine(degrees + " Fahrenheit are " + (((degrees-32)*5)/9) + " Celsius");
-                    Console.WriteLine(degrees + " Fahrenheit are " + ((((degrees-32)*5)/9)+273.15) + " Kelvin");
-                    break;
+                float degrees = float.Parse(GivenDegrees);
+                switch (Number)
+                {
+                    case 1:
+                        Console.WriteLine(degrees + " Celsius are " + Math.Round(degrees + 273.15, 2) + " Kelvin");
+                        Console.WriteLine(degrees + " Celsius are " + Math.Round(degrees * 1.8 + 32, 2) + " Fahrenheit");
+                        break;
+                    case 2:
+                        Console.WriteLine(degrees + " Kelvin are " + Math.Round(degrees - 273.15, 2) + " Celsius");
+                        Console.WriteLine(degrees + " Kelvin are " + Math.Round((degrees - 273.15) * 1.8 + 32, 2) + " Fahrenheit");
+                        break;
+                    case 3:
+                        Console.WriteLine(degrees + " Fahrenheit are " + Math.Round(((degrees - 32) * 5) / 9, 2) + " Celsius");
+                        Console.WriteLine(degrees + " Fahrenheit are " + Math.Round((((degrees - 32) * 5) / 9) + 273.15, 2) + " Kelvin");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("{0} is not an integer", GivenDegrees);
+                Console.WriteLine("Try again!");
+                Convert(Number);
+            }
+        }
+        static void Again()
+        {
+            Console.WriteLine("Do you want to try again?");
+            Console.WriteLine("'yes' or anything for no");
+            string answer = Console.ReadLine();
+            if (answer == "yes")
+            {
+                GetInfo();
+                Again();
+            }
+            else
+            {
+                Console.WriteLine("Thanks for using my converter");
             }
         }
     }
