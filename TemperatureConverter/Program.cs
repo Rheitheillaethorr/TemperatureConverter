1bd673d343d0e66668db2680439c9df62f3a3342
Rheitheillaethor <minegocu3@gmail.com>
2021-10-13 22:11:14 +0200

    Backup, Skel builded, I need to prevent against writing char because it breaks program

--- TemperatureConverter/Program.cs
+++ TemperatureConverter/Program.cs
@@ -6,7 +6,35 @@ namespace TemperatureConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GetInfo();
+            
+        }
+        static void GetInfo()
+        {
+            Console.WriteLine("Hello, what temperature scale do you want to convert from?");
+            Console.WriteLine("'1' - Celsius");
+            Console.WriteLine("'2' - Kelvin");
+            Console.WriteLine("'3' - Fahrenheit");
+            int choice = int.Parse(Console.ReadLine());      
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("You choosed Celsius");
+                    //Convert(choice);
+                    break;
+                case 2:
+                    Console.WriteLine("You choosed Kelvin");
+                    //Convert(choice);
+                    break;
+                case 3:
+                    Console.WriteLine("You choosed Fahrenheit");
+                    //Convert(choice);
+                    break;
+                default:
+                    Console.WriteLine("You choosed wrong number, try again");
+                    GetInfo();
+                    break;
+            }
         }
     }
 }
